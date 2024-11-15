eefbba4a779063f9f90cf40d4e9434c663d1647f
shariful-alam-2024 <outsourcingshorif2024@gmail.com>
2024-11-15 07:53:45 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,6 @@
+const colorDiv = document.getElementById('colorDiv');
+const colorButton = document.getElementById('colorButton');
+
+colorButton.addEventListener('click', () => {
+    colorDiv.style.backgroundColor = 'lightblue';
+});
